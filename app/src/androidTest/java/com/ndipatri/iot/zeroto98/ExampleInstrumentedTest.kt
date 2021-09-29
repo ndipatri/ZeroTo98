146d37e3da62849106a39d8d582dc973f028ff63
Nicholas DiPatri <ndipatri@gmail.com>
2021-09-29 12:35:01 -0400

    no message

--- app/src/androidTest/java/com/ndipatri/iot/zeroto98/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/ndipatri/iot/zeroto98/ExampleInstrumentedTest.kt
@@ -21,8 +21,13 @@ import okhttp3.OkHttpClient
 import okhttp3.mock.*
 
 import org.junit.Assert.*
-import org.junit.Rule
 import java.net.HttpURLConnection
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
